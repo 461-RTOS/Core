25219e6d81ed6e6f815c97280e3d20f4ba7c68c6
B33Mack <braedenmcmurray@gmail.com>
2025-02-24 13:58:04 -0700

    Update RTOS.c
    Added empty functions of what we want to make

--- RTOS.c
+++ RTOS.c
@@ -4,6 +4,70 @@
 *   This is a generic comment block that will tell us what the following function
 *   is supposed to do.
 *********************************************************************************/
-void ChangeTaskProperties(TaskHandle handle, TaskProperties properties){
+void CreateTask(func** point, int size, void** arg, TaskHandle handle){
                                                                             // This is a generic line comment to give more insight on specific lines
 }
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+void ChangeTaskProperties(TaskHandle handle, TaskProperties properties){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+void ToggleTaskSuspend(TaskHandle handle){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+bool IsTaskSuspended(TaskHandle handle){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+void DeleteTask(TaskHandle handle){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   Will recieve timer, just using int as placeholder
+*********************************************************************************/
+void OsInitialize(int timer){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+void OsStart(TaskHandle handle, TaskProperties properties){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+void OsStop(TaskHandle handle, TaskProperties properties){
+    // This is a generic line comment to give more insight on specific lines
+}
+
+/********************************************************************************
+*   This is a generic comment block that will tell us what the following function
+*   is supposed to do.
+*********************************************************************************/
+void OsKill(TaskHandle handle, TaskProperties properties){
+    // This is a generic line comment to give more insight on specific lines
+}
+
