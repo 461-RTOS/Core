9ef7715a8f481444c3f2bdece059ff0d9e0cb3a0
yugimumoto1 <sonnytoe96@gmail.com>
2025-03-05 15:27:32 -0700

    added function to contextSwitcher to check for interrupt sent by RTOS

--- contextSwitcher.c
+++ contextSwitcher.c
@@ -1,29 +1,30 @@
+#include "RTOS.h"
+#include "interrupt.h"
 #include "task.h"
 
+bool taskSwitchInterrupt;//will be true if new task is chosen
 
+void checkSwitch(void){
+    if (taskSwitchInterrupt == true){
+        switchHandler();
+    }
+}
 void switchHandler(void){
     /* 
-    if recieve: interrupt
-        call function: findMemoryAddress
-        insert into register 0: addressReturned
-        move all main registers: address in register 0
-        place: 0 in register 0
-        push: 0 into all available registers
-        block:pipeline
-        flush:pipeline
+    Following function saves a current task to be ran later and places a new task from the stack into the registers
     */
    __asm_volatile  (
    "    mrs r0,psp      \n"
-   "    isb             \n"
+   "    isb             \n" //stops pipeline
    "                    \n"
-   "    push {lr}       \n"
-   "    bl getCurrentTask   \n"
-   "    bl getSaveContextPtr    \n"
+   "    push {lr}       \n"// pushes line register 
+   "    bl getCurrentTask   \n"// branch links into RTOS function to get current task parameters
+   "    bl getSaveContextPtr    \n"//branch links into RTOS function to get save location and store to r0
    "                    \n"
-   "    stmdb r0!, {r4-r11, sp-lr}    \n"
-   "    bl selectNextTask   \n"
-   "    bl getLoadContextPtr   \n"
-   "    ldmfd r0!, {r4-r11, sp-lr}  \n"
-   "    pop {lr}    \n"
+   "    stmdb r0!, {r4-r11, sp-lr}    \n"//saves all usable registers to memory location
+   "    bl selectNextTask   \n" //branch link to RTOS function that selects next task to run
+   "    bl getLoadContextPtr   \n"//branch lin to RTOS function to get address to new task and store to r0
+   "    ldmfd r0!, {r4-r11, sp-lr}  \n"//places register context from memory location onto usable registers
+   "    pop {lr}    \n" //pops line register
    );
 }
--- contextSwitcher.h
+++ contextSwitcher.h
@@ -2,5 +2,6 @@
 #include "interrupt.h"
 #include "task.h"
 
+void checkSwitch(void);
 void switchHandler(void);
 
