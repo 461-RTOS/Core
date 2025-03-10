e798ee8d2820054950d92443af045a77c7d91642
Jakedez <99777297+Jakedez@users.noreply.github.com>
2025-03-10 13:59:35 -0600

    Fixed selectNextTask logic

--- task.c
+++ task.c
@@ -5,18 +5,21 @@
 static TaskControlBlock TCB;    // Empty, uninitialized Task Control Block
 
 TaskHandle selectNextTask(void){
-    int i = 0;
-    while (i != sizeof(TCB.tasks)){
+    
+    for (int i = 0; i != TCB.taskCount; i++){
         if (TCB.tasks[i]->lastRunTime < TCB.currentTask->lastRunTime)
             if(TCB.tasks[i]->User_Properties.priority > TCB.currentTask->User_Properties.priority)
                 return TCB.tasks[i];
-            
     }
-    return NULL;                    // Returning NULL until logic to select next task is implemented
+    return NULL;                    // Returning NULL if no task is Selected
 }
 
 TaskHandle getCurrentTask(void){
     return TCB.currentTask;
+}
+
+void setCurrentTask(TaskHandle task){
+    TCB.currentTask = task;
 }
 
 void * getSaveContextPtr(TaskHandle task){
